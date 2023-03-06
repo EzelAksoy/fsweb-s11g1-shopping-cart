d4ccd397e6c6a23fc779df0d3f4d53c847f21d7a
EzelAksoy <aksoy.ezel@gmail.com>
2023-03-06 17:31:30 +0300

    ödev tamamlandı.

--- src/App.js
+++ src/App.js
@@ -6,29 +6,40 @@ import { data } from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products, setProducts] = useState(data);
   const [cart, setCart] = useState([]);
-
   const addItem = (item) => {
+    if (!cart.includes(item) === true) {
+      setCart([...cart, item]);
+    }
     // verilen itemi sepete ekleyin
+  };
+  const removeItem = (item) => {
+    setCart([...cart.filter((i) => i.id !== item.id)]);
   };
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <CartContext.Provider value={{ cart, removeItem }}>
+        {" "}
+        <Navigation cart={cart} />
+        {/* Routelar */}
+        <ProductContext.Provider value={{ products, addItem }}>
+          <main className="content">
+            <Route exact path="/">
+              <Products />
+            </Route>
 
-      {/* Routelar */}
-      <main className="content">
-        <Route exact path="/">
-          <Products products={products} addItem={addItem} />
-        </Route>
-
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </main>
+            <Route path="/cart">
+              <ShoppingCart cart={cart} />
+            </Route>
+          </main>
+        </ProductContext.Provider>
+      </CartContext.Provider>
     </div>
   );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { ScProduct } from "./scParts";
+import { useContext } from "react";
+import { ProductContext } from "../contexts/ProductContext";
 
 const Product = (props) => {
   return (
